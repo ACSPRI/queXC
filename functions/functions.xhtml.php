847aeef871cd56775494e52f9af62e7abc6c808a
azammitdcarf <azammitdcarf>
2009-05-13 01:55:47 +0000

    Make sure XML displays as UTF-8

--- functions/functions.xhtml.php
+++ functions/functions.xhtml.php
@@ -55,6 +55,7 @@ function xhtml_script($scripts)
  */
 function xhtml_head($title="",$body=true,$css=false,$javascript=false,$bodytext=false,$refresh=false,$clearrefresh=false)
 {
+print "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 ?>
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
 	   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
